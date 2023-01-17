fecb7e5b744247f42bb0ba4ec34fe7963b6444f3
Ofir Salem <89575154+ofirsalem1@users.noreply.github.com>
2023-01-17 14:31:45 +0200

    test - 13 Component Testing

--- ofir-salem/13 - Component Testing/index.js
+++ ofir-salem/13 - Component Testing/index.js
@@ -1,5 +1,18 @@
+import classNames from 'classnames';
 import React from 'react';
 
-export default () => {
-  // Implement this component to pass the tests in ./__tests/index.spec.js
+export default ({ data = [], onClick }) => {
+  return (
+    <ul>
+      {data.map((element) => (
+        <li
+          key={element.key}
+          onClick={() => onClick(element.key)}
+          className={classNames({ selected: element.selected })}
+        >
+          {element.name}
+        </li>
+      ))}
+    </ul>
+  );
 };
